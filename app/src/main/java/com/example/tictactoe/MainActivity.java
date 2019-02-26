6deeea72416225878bfd47f53add7495fbc5fdb9
TheNerdThing <GregoryEvevsky@gmail.com>
2019-02-25 21:47:41 -0500

    added a check for ties and the computer winning

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -48,9 +48,16 @@ public class MainActivity extends AppCompatActivity {
                     get.setText("X");
                     // if we win stop the game
                     // otherwise make move still start an infinite loop
-                    if (checkVictory()) {
+                    if (checkVictory("X")) {
                         gameOver = true;
                         Toast.makeText(getApplicationContext(), "Congradulations! You win!", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    // check for tie and reset the game if that happens
+                    Log.d("Tic Tac Toe", "check for draw =  " + checkForDraw());
+                    if(checkForDraw()){
+                        gameOver = true;
+                        Toast.makeText(getApplicationContext(), "Its a draw", Toast.LENGTH_SHORT).show();
                         return;
                     }
                     nextMove();
@@ -77,21 +84,21 @@ public class MainActivity extends AppCompatActivity {
      * checks if there are 3 in a row on the board
      * @return
      */
-    public boolean checkVictory(){
+    public boolean checkVictory(String player){
         // check columns and rows
         for(int i = 0; i < board.length; i++){
-            if((board[i][1].getText() == board[i][0].getText() ) && ( board[i][0].getText() == board[i][2].getText())  && board[i][0].getText() != ""){
+            if((board[i][1].getText() == board[i][0].getText() ) && ( board[i][0].getText() == board[i][2].getText())  && board[i][0].getText().equals(player)){
                 return  true;
             }
-            if((board[1][i].getText() == board[0][i].getText() ) && board[0][i].getText() == board[2][i].getText() && board[2][i].getText() != ""){
+            if((board[1][i].getText() == board[0][i].getText() ) && board[0][i].getText() == board[2][i].getText() && board[2][i].getText().equals(player)){
                 return  true;
             }
         }
         // check diagonals
-        if (board[1][1].getText().equals(board[0][0].getText() )   &&  board[1][1].getText().equals(board[2][2].getText() )  && board[0][0].getText() != ""){
+        if (board[1][1].getText().equals(board[0][0].getText() )   &&  board[1][1].getText().equals(board[2][2].getText() )  && board[0][0].getText().equals(player)){
             return true;
         }
-        if (board[0][2].getText() == board[1][1].getText()  &&  board[1][1].getText() == board[2][0].getText() && board[1][1].getText() != ""){
+        if (board[0][2].getText() == board[1][1].getText()  &&  board[1][1].getText() == board[2][0].getText() && board[1][1].getText().equals(player)){
             return  true;
         }
         return  false;
@@ -102,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void nextMove(){
         boolean moveMade = false;
+        // find a valid move
         do {
             int rndX = (int) (Math.random() * 3);
             int rndY = (int) (Math.random() * 3);
@@ -111,6 +119,26 @@ public class MainActivity extends AppCompatActivity {
                 moveMade = true;
             }
         }while(!moveMade);
+        // check for victory
+        if(checkVictory("O")) {
+            gameOver = true;
+            Toast.makeText(getApplicationContext(), "Computer wins!", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
+    }
+
+    private  boolean checkForDraw(){
+        // try to find an empty space. return false if there is an empty space
+        for(int x = 0; x < board.length ; x++){
+            for (int y = 0 ; y < board[x].length ; y++){
+                if(board[x][y].getText().equals("")){
+                    Log.d("tic tac toe" , " empty space found at " + x  + " " + y);
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
